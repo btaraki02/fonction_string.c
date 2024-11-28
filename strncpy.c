2dc9b81a395f0adfea84d9c1f798906114132d3a
Taraki Bilal <your.email@example.com>
2024-11-28 18:43:18 +0100

    hihihi

--- strncpy.c
+++ strncpy.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
  char *ft_strncpy(char *dest, char *src, unsigned int n){
     int i = 0;
-    while(src[i] != '\0'&& i <n){ //difo kan f mo9aranat src[i] m3a n ++> khesk t9aren i be n  .
+    while(src[i] != '\0'&& i <n){
         
         dest[i] = src[i];
         i++;
