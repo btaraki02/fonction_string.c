1491ac192e8b50e8cde918eff2a3968af21cf061
Taraki Bilal <your.email@example.com>
2024-11-29 08:22:00 +0100

    ko

---
+++ strcat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ char *ft_strcat(char *dest, char *src){
+while(*dest != '\0'){
+    dest++;
+}
+
+while(*src != '\0'){
+    *dest = *src;
+    dest++;
+    src++;
+}
+*dest != '\0';
+return dest;
+
+ }
+
+ /*int main(){
+char momo[101];
+printf("enter your text:a ");
+gets(momo);
+char lolo[1010];
+printf("enter your text:b ");
+gets(lolo);
+char *src = momo;
+char *dest = lolo;
+ft_strcat(dest,src);
+printf("%s",lolo);
+return 0;
+ }
